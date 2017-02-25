4cd8edac2afbddd0134a857d1ca06bd0df2739f9
Tim Leavey <timothy.leavey@gmail.com>
2017-02-25 14:44:53 -0800

    Fixed #24. album.rb now raises an argument if rank parameter is less than 1.

--- album.rb
+++ album.rb
@@ -2,6 +2,8 @@ class Album
   attr_accessor :rank, :title, :year, :title_length
 
   def initialize(rank, title, year)
+    raise ArgumentError.new("Invalid rank") if rank < 1
+
     @rank = rank
     @title = title
     @year = year
@@ -11,6 +13,6 @@ class Album
   def get_title_length(title)
     title.size
   end
-  
+
 end
 
